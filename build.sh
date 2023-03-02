0dc5278e2f9719ed6525fa74f04d7da992f15b78
Rio Astamal <me@rioastamal.net>
2023-03-02 07:38:03 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+[ ! -f "./event-sqs.sample.json" ] && {
+    # Just to make sure we are in the right place
+    echo "Missing event-sqs.sample.json file, make sure you're in project root folder." >&2
+    exit 1
+}
+
+APP_NAME=serverless-todo-email-worker
+ZIP_FILE_NAME=${APP_NAME}.zip
+FILES_TO_ZIP="src/ node_modules/ lambda.js"
+SOURCE_FILES="src package.json package-lock.json"
+
+# Zip file will be uploaded to this bucket
+[ -z "$APP_FUNCTION_BUCKET" ] && {
+    echo "Missing APP_FUNCTION_BUCKET" 2>&1
+    exit 1
+}
+
+rm -rf .build/tmp/ 2>/dev/null
+
+echo "Downloading app dependencies..."
+mkdir -p .build/tmp
+cp -a $SOURCE_FILES .build/tmp/
+cd .build/tmp && npm install --omit=dev
+
+echo "Creating zip..."
+zip -q -r $ZIP_FILE_NAME $FILES_TO_ZIP
+
+cd ../..
+
+echo "Uploading zip to S3 Bucket..."
+aws s3 cp .build/tmp/$ZIP_FILE_NAME s3://$APP_FUNCTION_BUCKET/$ZIP_FILE_NAME
